aae596627787c4c52a9497dad4249f5c6039228b
anig1scur <anigiscur@gmail.com>
2025-02-25 21:13:21 -0600

    load meme data to radar

--- src/components/visualization/Sentiment.svelte
+++ src/components/visualization/Sentiment.svelte
@@ -2,43 +2,42 @@
   import {LayerCake, Svg} from 'layercake';
   import Radar from '../chart/Radar.svelte';
   import AxisRadial from '../chart/AxisRadial.svelte';
-
-  const data = [
-    {
-      name: 'Allison',
-      fastball: 10,
-      change: 0,
-      slider: 4,
-      cutter: 8,
-      curve: 5,
-    },
-  ];
+  import rawData from '/public/meme_stats.json';
 
   const seriesKey = 'name';
-  const xKey = ['fastball', 'change', 'slider', 'cutter', 'curve'];
+  const xKey = ['humour', 'sarcasm', 'offensive', 'motivational', 'sentiment'];
 
-  const seriesNames = Object.keys(data[0]).filter((d) => d !== seriesKey);
+  const data = rawData.slice(0, 9).map((d) => {
+    const [humour, sarcasm, offensive, motivational] = d.emotion;
+    const sentiment = (humour + motivational - sarcasm - offensive + 10) / 4;
 
-  data.forEach((d) => {
-    seriesNames.forEach((name) => {
-      d[name] = +d[name];
-    });
+    return {
+      ...d,
+      humour,
+      sarcasm,
+      offensive,
+      motivational,
+      sentiment,
+    };
   });
 </script>
 
-<div class="mt-16 flex justify-center items-center">
-  <div class="w-96 h-96">
-    <LayerCake
-      padding={{top: 10, right: 10, bottom: 10, left: 10}}
-      x={xKey}
-      xDomain={[0, 10]}
-      xRange={({width, height}) => [0, Math.min(width, height) / 2]}
-      {data}
-    >
-      <Svg>
-        <AxisRadial />
-        <Radar />
-      </Svg>
-    </LayerCake>
-  </div>
+<div class="font-routedgothic mt-28 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-12">
+  {#each data as item}
+    <div class="radar-chart">
+      <LayerCake
+        padding={{top: 20, right: 40, bottom: 20, left: 40}}
+        x={xKey}
+        xDomain={[0, 5]}
+        xRange={({width, height}) => [0, Math.min(width, height) / 2]}
+        data={[item]}
+      >
+        <Svg>
+          <AxisRadial />
+          <Radar />
+        </Svg>
+      </LayerCake>
+      <div class="meme-name">{item.name}</div>
+    </div>
+  {/each}
 </div>
